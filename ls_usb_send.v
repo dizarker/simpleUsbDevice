bb254cbe7c98dce268293e012f405392851a98a1
Dizarker <dizarker@gmail.com>
2019-01-12 01:10:43 +0300

    Коммит проекта и первого файла в нем

---
+++ ls_usb_send.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    01:08:25 01/12/2019 
+// Design Name: 
+// Module Name:    ls_usb_send 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ls_usb_send(
+    );
+
+
+endmodule
